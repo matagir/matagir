9d94f3f446cd3c455623c79bc6eac0897c3e459f
matagir <165258966+matagir@users.noreply.github.com>
2024-03-28 13:21:07 +0500

    Add files via upload

---
+++ 'homework4.py'.py
@@ -0,0 +1,9 @@
+immutable_var = ["кошка", "собака", 1, 2, 3, 4, 5]
+print(immutable_var)
+immutable_var[0] = "жираф"
+print(immutable_var)
+mutable_list = [1,2,3,4,5,6,7,8,9]
+mutable_list[4] = 23
+print(mutable_list)
+mutable_list.append('Los')
+print(mutable_list)
